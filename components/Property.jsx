c405d41f2cbe98923724f5bef0bcbdcf02a93ce8
GrethelRivera <grethelr91@gmail.com>
2022-11-01 22:32:33 -0400

    Commit -  TimeStamp: 46:19

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,13 +1,41 @@
 import { Box, Flex, Spacer, Text } from '@chakra-ui/layout';
+import Link from 'next/link'
+import Image from 'next/image';
 import { Avatar } from '@chakra-ui/avatar';
 import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
+import next from 'next';
+
+import DefaultImage from '../assets/images/house.jpg';
 
 const Property = ({property: {coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerifies, externalId}}) => (
     <Link href={`/property/${externalId}`} passHref>
-    {title}
+    <Flex flexWrap="wrap" w="420x" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
+        <Box>
+            <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="house"/>  
+        </Box>
+        <Box w="full">
+            <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+                <Flex alignItems = "center">
+                    <Box paddingRight="3" color="green.400">{isVerified && <GoVerified/>}</Box>
+                <Text fontweight ="bold" fontSize="lg">AED{millify(price)} {rentFrequency && `/${rentFrequency}`}</Text>
+                </Flex>
+                <Box>
+                    <Avatar size='sm' src={agency?.logo?.url } />
+                </Box>
+             </Flex>
+             <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400">
+                {rooms} <FaBed /> |{baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+             </Flex>
+            <Text fontSize="lg" >
+                {title.length > 30 ? `${title.substring(1, 30)}}...` : title}
+
+            </Text>
+</Box>
+    </Flex>
+
    </Link>
 );
 
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -12,3 +12,4 @@ headers: {
 
 return data;
 }
+
